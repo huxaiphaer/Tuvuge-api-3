b675df318746468aaac816dda7f1a60367e5bd34
huxaiphaer <huxaiphaeridris@gmail.com>
2018-07-02 15:24:51 +0300

    fetch all ride requests by id

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from app.rides.managerides import GetRides,GetSingleRide,CreateRideRequests
+from app.rides.managerides import GetRides, GetSingleRide, CreateRideRequests, GetRideOfferRequests
 #from app.user.authentication import Login, SignUp
 #from app.rides.request_ride_offer import RequestRideOffer
 
@@ -13,6 +13,7 @@ api = Api(app)
 
 #api.add_resource(SignUp, '/api/v1/signup')
 #api.add_resource(Login, '/api/v1/login')
+api.add_resource(GetRideOfferRequests,'/api/v1/users/rides/<rideId>/requests')
 api.add_resource(GetRides, '/api/v1/users/rides')
 api.add_resource(GetSingleRide, '/api/v1/rides/<ride_id>')
 api.add_resource(CreateRideRequests, '/api/v1/<rideoffer_id>/requests')
