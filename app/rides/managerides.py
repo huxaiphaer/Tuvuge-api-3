8b4592ad78ec0ec5bbba93242e190e191d79be3a
huxaiphaer <huxaiphaeridris@gmail.com>
2018-07-04 19:50:23 +0300

    add test logic

--- app/rides/managerides.py
+++ app/rides/managerides.py
@@ -325,13 +325,13 @@ class AcceptOrRejectOffer(Resource):
                     cur.execute(
                         "update  requests SET status  ='"+status+"' where ride_offer_id = '"+rideId+"' ")
                     return make_response(jsonify({"message": "ride request  rejected"}),
-                                        201)
+                                        200)
                 else:
                     cur = con.cursor()
                     cur.execute(
                         "update  requests SET status  ='"+status+"' where ride_offer_id = '"+rideId+"' ")
                     return make_response(jsonify({"message": "ride request  accepted"}),
-                                        201)
+                                        200)
             return make_response(jsonify({"message": "ride offer is not found please "}),
                                 404)
 
