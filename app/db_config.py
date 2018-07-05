901393a67ebd77cc5893e200f8f61b6550b5ecc3
huxaiphaer <huxaiphaeridris@gmail.com>
2018-07-05 23:24:39 +0300

    added more test logic

--- app/db_config.py
+++ app/db_config.py
@@ -4,8 +4,9 @@ import psycopg2
 import sys
 import os
 
-#DATABASE_URL = os.environ['postgres://yffaefhpyvbwkl:51460cce946c9ee2a98c6ef8e71e7e193b7e14bb3d0f8c26d8fe02bf5eb91dd6@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d3fibeu3klt9e7']
+# DATABASE_URL = os.environ['postgres://yffaefhpyvbwkl:51460cce946c9ee2a98c6ef8e71e7e193b7e14bb3d0f8c26d8fe02bf5eb91dd6@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d3fibeu3klt9e7']
 con = None
+
 con = psycopg2.connect(
     "host='localhost' dbname='ride_my_way' user='postgres' password='namungoona'")
 # con = psycopg2.connect(
